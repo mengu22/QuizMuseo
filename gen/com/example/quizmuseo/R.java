f7feb5a1d5bdc72526961612aa584dd6ede88891
mengu22 <adrianmengual22@gmail.com>
2014-11-05 22:34:53 +0100

    Strings
    He puesto todos los elemntos de la interfaz con sus respectivos Strings

--- gen/com/example/quizmuseo/R.java
+++ gen/com/example/quizmuseo/R.java
@@ -1268,7 +1268,21 @@ containing a value of this type.
         public static final int abc_shareactionprovider_share_with_application=0x7f0a000b;
         public static final int action_settings=0x7f0a000f;
         public static final int app_name=0x7f0a000d;
+        public static final int back=0x7f0a001a;
         public static final int hello_world=0x7f0a000e;
+        public static final int insert=0x7f0a001c;
+        public static final int load=0x7f0a0011;
+        public static final int opc=0x7f0a001d;
+        public static final int play=0x7f0a001b;
+        public static final int pregunta=0x7f0a0014;
+        public static final int respa=0x7f0a0015;
+        public static final int respb=0x7f0a0016;
+        public static final int respc=0x7f0a0017;
+        public static final int respcorr=0x7f0a0018;
+        public static final int salir=0x7f0a0010;
+        public static final int save=0x7f0a0019;
+        public static final int tema=0x7f0a0013;
+        public static final int tipo=0x7f0a0012;
     }
     public static final class style {
         /** 
